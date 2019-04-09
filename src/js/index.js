8c604e96be3eb3b9c40f0e0c486aecb8228ea07b
Shilpa Rai <shirai@publicisgroupe.net>
2019-04-09 12:40:49 +0530

    data

--- src/js/index.js
+++ src/js/index.js
@@ -3,31 +3,28 @@ var data = require("./handlebars.js");
 var ourRequest = new XMLHttpRequest();
 ourRequest.open('GET', 'data/products/index.get.json');
 ourRequest.onload = function() {
-  console.log(ourRequest);
-  if (ourRequest.status >= 200 && ourRequest.status < 400) {
-    // This is where we'll do something with the retrieved data
-    var data = JSON.parse(ourRequest.responseText);
-    console.log(data);
-    createHTML(data);
-  } else {
-    console.log("We connected to the server, but it returned an error.");
-  }
+    console.log(ourRequest);
+    if (ourRequest.status >= 200 && ourRequest.status < 400) {
+        // This is where we'll do something with the retrieved data
+        var data = JSON.parse(ourRequest.responseText);
+        console.log(data);
+        createHTML(data);
+    } else {
+        console.log("We connected to the server, but it returned an error.");
+    }
 };
 
 ourRequest.onerror = function() {
-  console.log("Connection error");
+    console.log("Connection error");
 };
 
 ourRequest.send();
 
 function createHTML(petsData) {
-  var rawTemplate = document.getElementById("student-template").innerHTML;
-  var compiledTemlate = data.compile(rawTemplate);
-  var ourData = compiledTemlate(petsData);
+    var rawTemplate = document.getElementById("student-template").innerHTML;
+    var compiledTemlate = data.compile(rawTemplate);
+    var ourData = compiledTemlate(petsData);
 
-  var petsContainer = document.getElementById("product-name");
-  petsContainer.innerHTML = ourData;
+    var petsContainer = document.getElementById("product-name");
+    petsContainer.innerHTML = ourData;
 }
-
-
- 
